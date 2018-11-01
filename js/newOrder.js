629105ce48ca363f75c7ccf9da338733f3ea37c7
Luc.A <luc.bayo@gmail.com>
2018-11-01 18:47:19 +0200

    complited user dashboard, and admin dashboar, responsiviness 90%. Remaining new order form

--- js/newOrder.js
+++ js/newOrder.js
@@ -1,15 +1,34 @@
-const nextForm = () => {
-	let orderForms = [...document.getElementsByClassName("order-form")];
-	let steps = [...document.getElementById("progressbar").children];
+//get all forms with order-form class
+let orderForms = [...document.getElementsByClassName("order-form")];
 
-	let activeSteps = steps.filter(step => step.classList.contains("active"));
+//get the progressbar's chrildren => steps
+let steps = [...document.getElementById("progressbar").children];
+
+const nextOrPrev = action => {
+	//get the current form's index
 	let activeIndex = orderForms.indexOf(
 		orderForms.find(form => form.classList.contains("active"))
 	);
+
+	//filter steps, to get only activated one.
+	let activeSteps = steps.filter(step => step.classList.contains("active"));
+	//desactivate the current form, and activate the next one
 	orderForms[activeIndex].classList.remove("active");
-	orderForms[activeIndex + 1].classList.add("active");
-	console.log(activeSteps);
-	if (steps.length > activeSteps.length) {
-		steps[activeSteps.length].classList.add("active");
+
+	// manupulate the next or pevious form and step
+
+	switch (action) {
+		case "next":
+			orderForms[activeIndex + 1].classList.add("active");
+			steps.length > activeSteps.length &&
+				steps[activeSteps.length].classList.add("active");
+			break;
+		case "prev":
+			orderForms[activeIndex - 1].classList.add("active");
+
+			steps.length > activeSteps.length &&
+				steps[activeSteps.length].classList.remove("active");
+		default:
+			return null;
 	}
 };
